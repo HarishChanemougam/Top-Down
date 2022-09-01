748fc28b76ddcd59d0fd7edb121ff66737e5ea06
Harish <harish.chanemougam@3wa.io>
2022-09-01 17:31:25 +0200

    EnvironmentAndScriptUpdate

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] InputActionReference _moveInput;
+    [SerializeField] Transform _root;
+    [SerializeField] Animator _animator;
+    [SerializeField] float _movingThreshold;
+    [SerializeField] float _speed;
+
+    Vector2 _playerMovement;
+
+
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        _root = transform.parent;
+        _speed = 5f;
+        Debug.Log("reset");
+    }
+#endif
+
+    private void Start()
+    {
+        _moveInput.action.started += StartMove;
+        _moveInput.action.performed += UpdateMove;
+        _moveInput.action.canceled += EndMove;
+    }
+
+
+    private void Update()
+    {
+        
+        Vector2 direction = new Vector2(_playerMovement.x, 0);
+        _root.transform.Translate(_playerMovement * Time.deltaTime * _speed, Space.World);
+
+
+        
+        if (direction.magnitude > _movingThreshold) 
+        {
+            _animator.SetBool("IsWalking", true);
+
+        }
+
+        else
+        {
+            _animator.SetBool("IsWalking", false); 
+        }
+
+        if (direction.x > 0) 
+        {
+            _root.rotation = Quaternion.Euler(0, 0, 0);
+
+            
+        }
+
+        else if (direction.x < 0) 
+        {
+            _root.rotation = Quaternion.Euler(0, 180, 0);
+ 
+        }
+
+    }
+
+    private void StartMove(InputAction.CallbackContext obj)
+    {
+        _playerMovement = obj.ReadValue<Vector2>();
+
+    }
+
+    private void UpdateMove(InputAction.CallbackContext obj)
+    {
+        _playerMovement = obj.ReadValue<Vector2>();
+       
+    }
+
+    private void EndMove(InputAction.CallbackContext obj)
+    {
+        _playerMovement = new Vector2(0, 0);
+        
+    }
+}
